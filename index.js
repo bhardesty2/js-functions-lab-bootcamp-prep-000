d3a7fac420e03925def608e81e6dc31ae5e75bb2
Blake Hardesty <ha1003218@email.ccbcmd.edu>
2018-12-31 19:59:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday){
   return "Happy Independence Day, you!"
 }
+
+function holidayCountdown('holiday' 'days'){
+return "It's 21 days until Independence Day!"
+}
